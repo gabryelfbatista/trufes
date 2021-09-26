30d96ea3fb61e5afe398c5c6fa1bf71355a5ef18
Gabryel Batista <78879283+gabryelfbatista@users.noreply.github.com>
2021-09-25 21:38:28 -0300

    Add files via upload

--- src/components/carousel/cards.tsx
+++ src/components/carousel/cards.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
 
-export function Card(title:any, description:any){
-    const style = "grid flex w-4/5 h-quadrados"
+export type cardContent = {
+    title:string,
+    description:string,
+    color:string
+}
+
+export default function Card({title,description,color}:cardContent){
+    const style = "grid w-cards h-quadrados "
     return (
-        <div className = {style}>
-            <h1>{title}</h1>
-            <p>{description}</p>
+        <div className = {style+color}>
+            <h1 className="place-self-center text-center self-end mb-12 text-5xl font-bold text-white">
+                {title}
+                <p className="p-6 text-2xl font-normal"> {description} </p>
+            </h1>
         </div>
     )
 }
--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,15 +1,22 @@
-import {Card} from "./cards"
-import React from 'react'
+import Card from './cards'
+import {cardContent} from "./cards"
 
+type Props ={
+  data:cardContent[]
+}
 
-export function Carousel(props:any) {
-  const data = props
-
+export default function Carousel({data}:Props) { 
   return(
     <>
       <p className="text-center md:text-center mb-10 text-white text-9x1"> Categorias </p>
-      <div className="grid grid-cols-3 place-items-center justify-center mt-20 mx-12 h-retangulo bg-gray-500">
-        <div>{data.map((elementoAtual:any) => <Card title={elementoAtual.title} description={elementoAtual.description}></Card>)}</div>
+      <div className="grid place-items-center justify-items-center mt-20 mx-12 h-retangulo bg-gray-trufes">
+        <div className="grid grid-flow-col my-12 mx-16 space-x-40">
+          {data.map(elementoAtual => (
+            <div key = {elementoAtual.title} >
+              <Card title = {elementoAtual.title} description = {elementoAtual.description} color={elementoAtual.color}></Card>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )

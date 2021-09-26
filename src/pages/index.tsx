2228eb29356767ddfbd4f13399a5b330e1dd1ee7
Gabryel Batista <78879283+gabryelfbatista@users.noreply.github.com>
2021-09-25 21:38:56 -0300

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,27 @@ import { useRouter } from 'next/router';
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 
-import {Carousel} from '../components/carousel/carousel'
+import Carousel from '../components/carousel/carousel'
+import Card from '../components/carousel/cards'
 
 const carouselData = 
-[{title: 'vsss',
-description:'evento vsss'},
-{title:'Futebol2D',
-description:'Futebol mas 2D'}
+[{title: 'Futebol 2D',
+description:'escreva aqui',
+color: 'bg-green-trufes'},
+{title: 'OPEN',
+description:'escreva aqui',
+color: 'bg-blue-trufes'
+},
+{title: 'VSSS',
+description: 'escreva aqui',
+color: 'bg-orange-trufes'},
 ]
 
 const Index = () => {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen bg-black" > 
+    <div className="min-h-screen bg-black-trufes" > 
       <Carousel data={carouselData}/>
     </div>
   )
